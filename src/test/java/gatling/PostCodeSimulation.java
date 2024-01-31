5cd056d73b0a4dc4f0e545a9e41ee4d7855896bb
Anthony McGlone <anthonymcglone2022@gmail.com>
2024-01-31 12:37:55 +0000

    Logging option

--- src/test/java/gatling/PostCodeSimulation.java
+++ src/test/java/gatling/PostCodeSimulation.java
@@ -6,38 +6,36 @@ import io.gatling.javaapi.http.*;
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
-public class PostCodeSimulation extends Simulation { // 3
+public class PostCodeSimulation extends Simulation {
 	
-	
-  //String URL = "http://localhost:9191/checkViaDatabase/W5 1AT";
   String URL = "http://localhost:9191";
+  int limit = 3000;
+  int usersPerSecond = 5;
+  int maxDurationSeconds = 5;
+  String csv = "postcode_feeder.csv";
 
-  HttpProtocolBuilder httpProtocol = http // 4
-    .baseUrl(URL) // 5
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // 6
+  HttpProtocolBuilder httpProtocol = http
+    .baseUrl(URL)
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .doNotTrackHeader("1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0");
 
-  ScenarioBuilder scn = scenario("PostCodeSimulation") // 7
-    .feed(csv("postcode_feeder.csv").circular())
+  ScenarioBuilder scn = scenario("(n)-users per second checking postcodes")
+    .feed(csv(csv).circular())
 	.exec(
-    		http("checkViaDatabase") // 8
-    		.get("/checkViaDatabase/" + "${post_code}")
-    		.check(status().is(200))
-    		//.check(bodyString().saveAs("responseBody"))
-    )
-    .exec(session -> {
-        //System.out.println("Response Body:");
-        //System.out.println(session.getString("responseBody"));
-        return session;
-    })    
-    .pause(5); // 10
+		http("checkViaDatabase")
+		.get("/checkViaDatabase/" + "#{post_code}")
+		.check(status().is(200))	
+    )  
+    .pause(5);
 
   {
-    setUp( // 11
-      scn.injectOpen(constantUsersPerSec(5).during(5)) // 12
-    ).protocols(httpProtocol); // 13
+    setUp(
+      scn.injectOpen(constantUsersPerSec(usersPerSecond).during(maxDurationSeconds))
+    )
+    .assertions(global().responseTime().mean().lt(limit))
+    .protocols(httpProtocol);
   }
 }
